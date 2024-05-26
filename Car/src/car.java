3f816e05a5a664b7ca61478206a219276255684d
mmestres <mmestres@marks-mbp>
2024-05-25 22:09:43 -0400

    My first commit

---
+++ Car/src/car.java
@@ -0,0 +1,44 @@
+
+//*****************************************************************************************************************************************
+//   
+//	car.java			Author: Mark Mestres
+//
+//  Design a Java program that has a class called Car that contains instance data that represents the make, model, and year of the car.
+//
+//*****************************************************************************************************************************************
+
+
+public class car {
+	
+	public static void main(String[] args)
+	{
+		private string make;
+		
+		private string model;
+		
+		private int year;
+		
+		public car()
+		{
+			make = "Audi";
+			
+			model = "A4";
+			
+			year = 2018;
+		}
+	}
+	//Define the car constructor to initialize these values.
+	
+	//Include getter and setter methods for all instance data, and a toString method that returns a one-line description of the car. 
+
+	//Add a method called isAntique that takes current year as a parameter and returns a Boolean indicating if the car is antique (if it is more than 45 years old). 
+
+	//Create a driver class called CarTest, whose main method instantiates and updates four Car objects. Print the car descriptions for all the objects using the toString method. 
+
+	//Show use of getter and setter methods and print the objects again using toString method. For the current year (2024) check if the car objects created are antique or not and print a message in either case.    	
+	
+	
+	
+}
+
+
