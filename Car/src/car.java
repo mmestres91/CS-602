075085101cd1b599a8aa449cdbe8ba0aa7c1a894
mmestres <mmestres@marks-mbp>
2024-05-25 23:05:05 -0400

    Added carTest class

--- Car/src/car.java
+++ Car/src/car.java
@@ -12,6 +12,7 @@ public class car {
 	
 	public static void main(String[] args)
 	{
+		//Define the car constructor to initialize these values.
 		private string make;
 		
 		private string model;
@@ -26,18 +27,10 @@ public class car {
 			
 			year = 2017;
 		}
-	}
-	//Define the car constructor to initialize these values.
-	
-	//Include getter and setter methods for all instance data, and a toString method that returns a one-line description of the car. 
+		
+		//Include getter and setter methods for all instance data, and a toString method that returns a one-line description of the car. 
 
-	//Add a method called isAntique that takes current year as a parameter and returns a Boolean indicating if the car is antique (if it is more than 45 years old). 
-
-	//Create a driver class called CarTest, whose main method instantiates and updates four Car objects. Print the car descriptions for all the objects using the toString method. 
-
-	//Show use of getter and setter methods and print the objects again using toString method. For the current year (2024) check if the car objects created are antique or not and print a message in either case.    	
-	
-	
+		//Add a method called isAntique that takes current year as a parameter and returns a Boolean indicating if the car is antique (if it is more than 45 years old). 
 	
 }
 
---
+++ Car/src/carTest.java
@@ -0,0 +1,23 @@
+
+//*****************************************************************************************************************************************
+//   
+//	carTest.java			Author: Mark Mestres
+//
+//  Design a Java program that has a class called Car that contains instance data that represents the make, model, and year of the car.
+//
+//*****************************************************************************************************************************************
+
+
+public class carTest extends car {
+	public static void main(String[] args)
+	{
+	//Create a driver class called CarTest, whose main method instantiates and updates four Car objects. Print the car descriptions for all the objects using the toString method. 
+		public carTest()
+		{
+			//Show use of getter and setter methods and print the objects again using toString method. For the current year (2024) check if the car objects created are antique or not and print a message in either case.    
+
+		}
+	}
+}
+
+
