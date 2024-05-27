b05cd255fd0ca3133d41a1fa5c447fc6f36f298c
mmestres <mmestres@marks-mbp>
2024-05-27 13:00:29 -0400

    Complete code

--- Car/src/car.java
+++ Car/src/car.java
@@ -1,4 +1,3 @@
-
 //*****************************************************************************************************************************************
 //   
 //	car.java			Author: Mark Mestres
@@ -8,30 +7,53 @@
 //*****************************************************************************************************************************************
 
 
+
 public class car {
-	
-	public static void main(String[] args)
-	{
-		//Define the car constructor to initialize these values.
-		private string make;
-		
-		private string model;
-		
-		private int year;
-		
-		public car()
-		{
-			make = "Audi";
-			
-			model = "A4";
-			
-			year = 2017;
-		}
-		
-		//Include getter and setter methods for all instance data, and a toString method that returns a one-line description of the car. 
+    private String make;
+    private String model;
+    private int year;
 
-		//Add a method called isAntique that takes current year as a parameter and returns a Boolean indicating if the car is antique (if it is more than 45 years old). 
-	
+	//Define the car constructor to initialize these values.
+    public car(String make, String model, int year) {
+        this.make = make;
+        this.model = model;
+        this.year = year;
+    }
+
+	//Include getter and setter methods for all instance data, 
+    public String getMake() {
+        return make;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    // and a toString method that returns a one-line description of the car.
+    @Override
+    public String toString() {
+        return "Car [Make=" + make + ", Model=" + model + ", Year=" + year + "]";
+    }
+
+	//Add a method called isAntique that takes current year as a parameter and returns a Boolean indicating if the car is antique (if it is more than 45 years old). 
+    public boolean isAntique(int currentYear) {
+        return (currentYear - this.year) > 45;
+    }
 }
-
 
--- Car/src/carTest.java
+++ Car/src/carTest.java
@@ -1,4 +1,3 @@
-
 //*****************************************************************************************************************************************
 //   
 //	carTest.java			Author: Mark Mestres
@@ -7,17 +6,37 @@
 //
 //*****************************************************************************************************************************************
 
+public class carTest {
+    public static void main(String[] args) {
+    	//Create a driver class called CarTest, whose main method instantiates and updates four Car objects. Print the car descriptions for all the objects using the toString method. 
+        car car1 = new car("Ford", "Mustang", 1965);
+        car car2 = new car("Chevrolet", "Camaro", 1990);
+        car car3 = new car("Toyota", "Corolla", 2005);
+        car car4 = new car("Honda", "Civic", 2010);
 
-public class carTest extends car {
-	public static void main(String[] args)
-	{
-	//Create a driver class called CarTest, whose main method instantiates and updates four Car objects. Print the car descriptions for all the objects using the toString method. 
-		public carTest()
-		{
-			//Show use of getter and setter methods and print the objects again using toString method. For the current year (2024) check if the car objects created are antique or not and print a message in either case.    
+        System.out.println("Car descriptions:");
+        System.out.println(car1);
+        System.out.println(car2);
+        System.out.println(car3);
+        System.out.println(car4);
 
-		}
-	}
+        car1.setMake("Ford Updated");
+        car2.setModel("Camaro Updated");
+        car3.setYear(2015);
+        car4.setYear(1980);
+
+        System.out.println("\nUpdated car descriptions:");
+        System.out.println(car1);
+        System.out.println(car2);
+        System.out.println(car3);
+        System.out.println(car4);
+
+		//Show use of getter and setter methods and print the objects again using toString method. For the current year (2024) check if the car objects created are antique or not and print a message in either case.    
+        int currentYear = 2024;
+        System.out.println("\nAntique status:");
+        System.out.println(car1 + " is antique? " + car1.isAntique(currentYear));
+        System.out.println(car2 + " is antique? " + car2.isAntique(currentYear));
+        System.out.println(car3 + " is antique? " + car3.isAntique(currentYear));
+        System.out.println(car4 + " is antique? " + car4.isAntique(currentYear));
+    }
 }
-
-
